86d7bb58acf9df8b0b350600e07dc3faff27f221
ivanberan <51275831+ivanberan@users.noreply.github.com>
2020-01-30 11:08:05 +0100

    test

--- app/src/main/java/com/example/moje/upi_vs/settings.java
+++ app/src/main/java/com/example/moje/upi_vs/settings.java
@@ -2,6 +2,7 @@ package com.example.moje.upi_vs;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,11 +10,18 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class settings extends AppCompatActivity {
 
     private Spinner spinnerunit,spinnerlanguage;
     private TextView tvUnit,tvLang;
     private Button btnUpdate;
+    private Button btnDodaj, btnIzbrisi,btnProvjeri;
     private String langu="";
     private String Unit="°C";
 
@@ -31,9 +39,65 @@ public class settings extends AppCompatActivity {
         tvUnit=(TextView)findViewById(R.id.tvUnit);
         tvLang=(TextView)findViewById(R.id.tvLang);
         btnUpdate=(Button)findViewById(R.id.btnUpdate);
+        btnDodaj=findViewById(R.id.btnDodaj);
+        btnIzbrisi=findViewById(R.id.btnIzbrisi);
+        btnProvjeri=findViewById(R.id.btnProvjeri);
 
         setjezik(langu);
 
+        btnDodaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+                rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        rootRef.child("Test").setValue("Test");
+                        btnDodaj.setText("Dodano");
+
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+            }
+        });
+        btnProvjeri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+                rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild("Test")){
+                            btnProvjeri.setText("Provjereno");
+                            ;}
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+            }
+        });
+
+        btnIzbrisi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+                rootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild("Test")){
+                            rootRef.child("Test").removeValue();
+                            btnIzbrisi.setText("Izbrisano");
+                            ;}
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+            }
+        });
 
         spinnerunit = (Spinner)findViewById(R.id.unit);
         ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter
